4239a7538450942182226192c2cc16119fe28a16
Kunal <patelkr07@gmail.com>
2019-06-05 18:24:04 -0500

    added taco.js

---
+++ models/taco.js
@@ -0,0 +1,29 @@
+var orm = require("../config/orm.js");
+
+var taco = {
+    all: function(cb) {
+        orm.all("tacos", function(res) {
+            cb(res);
+        });
+    },
+
+    create: function(cols, vals, cb) {
+        orm.create("tacos", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+
+    update: function(objColVals, condition, cb) {
+        orm.update("tacos", objColVals, condition, function(res) {
+            cb(res);
+        })
+    },
+
+    delete: function(condition, cb) {
+        orm.delete("tacos", condition, funtion(res) {
+            cb(res);
+        });
+    }
+};
+
+module.exports = taco;
